2612c594c9cbb6541175d5d9bb853ee7097b45f4
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2024-09-19 23:30:16 -0400

    added second week files

---
+++ Week 2/Sample project/Sample project/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
