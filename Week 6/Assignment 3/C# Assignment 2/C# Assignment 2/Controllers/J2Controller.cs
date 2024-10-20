f820215c19e096b6ecaabd6f25a78aa2346e1ebb
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2024-10-20 18:11:02 -0400

    update

---
+++ Week 6/Assignment 3/C# Assignment 2/C# Assignment 2/Controllers/J2Controller.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C__Assignment_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class J2Controller : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ingridents"></param>
+        /// <returns></returns>
+        [HttpGet(template: "ChiliPeppers&Ingredients={ingridents}")]
+        public int TotalSpicines(string ingridents)
+        {
+            Dictionary<string, int> peppers = new Dictionary<string, int>
+            {
+               { "Poblano",1500 },
+               { "Mirasol",6000 },
+               { "Serrano",15500 },
+               { "Cayenne",40000 },
+               { "Thai",75000 },
+               { "Habanero",125000 }
+            };
+            string[] pepperslist = ingridents.Split(',');
+            int total = 0;
+            foreach (var pepper in pepperslist)
+            {
+                if (peppers.ContainsKey(pepper))
+                {
+                    total += peppers[pepper];
+                }
+            }
+            return total;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="P"></param>
+        /// <param name="N"></param>
+        /// <param name="R"></param>
+        /// <returns></returns>
+        [HttpGet(template: "Epidemiology")]
+        public int Tdays(int P, int N, int R)
+        {
+            int totalInfected = N;
+            int dailyInfected = N;
+            for (int day = 1; totalInfected <= P; day++)
+            {
+                dailyInfected *= R;
+                totalInfected += dailyInfected;
+
+                if (totalInfected > P)
+                {
+                    return day;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
