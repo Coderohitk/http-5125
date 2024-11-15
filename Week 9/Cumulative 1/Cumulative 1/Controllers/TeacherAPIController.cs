e6b2f8a0f2d974e04c414a33ed2383e759c33b7e
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2024-11-14 21:31:26 -0500

    cumulative added

---
+++ Week 9/Cumulative 1/Cumulative 1/Controllers/TeacherAPIController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Cumulative_1.Models;
+using System;
+using MySql.Data.MySqlClient;
+
+namespace Cumulative_1.Controllers
+{
+    [Route("api/Teacher")]
+    [ApiController]
+    public class TeacherAPIController : ControllerBase
+    {
+        private readonly Cumulative1dbContext _context;
+        // dependency injection of database context
+        public TeacherAPIController(Cumulative1dbContext context)
+        {
+            _context = context;
+        }
+
+
+        /// <summary>
+        /// Returns a list of Authors in the system
+        /// </summary>
+        /// <example>
+        /// GET api/Author/ListAuthors -> [{"AuthorId":1,"AuthorFname":"Brian", "AuthorLName":"Smith"},{"AuthorId":2,"AuthorFname":"Jillian", "AuthorLName":"Montgomery"},..]
+        /// </example>
+        /// <returns>
+        /// A list of author objects 
+        /// </returns>
+        [HttpGet]
+        [Route(template: "ListTeachers")]
+        public List<Teacher> ListTeachers()
+        {
+            // Create an empty list of Authors
+            List<Teacher> Teachers = new List<Teacher>();
+
+            // 'using' will close the connection after the code executes
+            using (MySqlConnection Connection = _context.AccessDatabase())
+            {
+                Connection.Open();
+                //Establish a new command (query) for our database
+                MySqlCommand Command = Connection.CreateCommand();
+
+                //SQL QUERY
+                Command.CommandText = "select * from teachers";
+
+                // Gather Result Set of Query into a variable
+                using (MySqlDataReader ResultSet = Command.ExecuteReader())
+                {
+                    //Loop Through Each Row the Result Set
+                    while (ResultSet.Read())
+                    {
+                        //Access Column information by the DB column name as an index
+                        int Id = Convert.ToInt32(ResultSet["teacherid"]);
+                        string FirstName = ResultSet["teacherfname"].ToString();
+                        string LastName = ResultSet["teacherlname"].ToString();
+                        string TeacherEmpNu= ResultSet["employeenumber"].ToString();
+
+                        DateTime TeacherHireDate = Convert.ToDateTime(ResultSet["hiredate"]);
+                        string TeacherSalary = ResultSet["salary"].ToString();
+
+                        //short form for setting all properties while creating the object
+                        Teacher CurrentTeacher = new Teacher()
+                        {
+                            TeacherId = Id,
+                            TeacherFName = FirstName,
+                            TeacherLName = LastName,
+                            TeacherHireDate = TeacherHireDate,
+                            TeacherSalary = TeacherSalary,
+                            TeacherEmpNu= TeacherEmpNu
+                        };
+
+                        Teachers.Add(CurrentTeacher);
+
+                    }
+                }
+            }
+
+
+            //Return the final list of authors
+            return Teachers;
+        }
+
+
+        /// <summary>
+        /// Returns an author in the database by their ID
+        /// </summary>
+        /// <example>
+        /// GET api/Author/FindAuthor/3 -> {"AuthorId":3,"AuthorFname":"Sam","AuthorLName":"Cooper"}
+        /// </example>
+        /// <returns>
+        /// A matching author object by its ID. Empty object if Author not found
+        /// </returns>
+        [HttpGet]
+        [Route(template: "FindTeacher/{id}")]
+        public Teacher FindTeacher(int id)
+        {
+
+            //Empty Author
+            Teacher SelectedTeacher = new Teacher();
+
+            // 'using' will close the connection after the code executes
+            using (MySqlConnection Connection = _context.AccessDatabase())
+            {
+                Connection.Open();
+                //Establish a new command (query) for our database
+                MySqlCommand Command = Connection.CreateCommand();
+
+                // @id is replaced with a 'sanitized' id
+                Command.CommandText = "select * from teachers where teacherid=@id";
+                Command.Parameters.AddWithValue("@id", id);
+
+                // Gather Result Set of Query into a variable
+                using (MySqlDataReader ResultSet = Command.ExecuteReader())
+                {
+                    //Loop Through Each Row the Result Set
+                    while (ResultSet.Read())
+                    {
+                        //Access Column information by the DB column name as an index
+                        int Id = Convert.ToInt32(ResultSet["teacherid"]);
+                        string FirstName = ResultSet["teacherfname"].ToString();
+                        string LastName = ResultSet["teacherlname"].ToString();
+                        string TeacherEmpNu = ResultSet["employeenumber"].ToString();
+
+                        DateTime TeacherHireDate = Convert.ToDateTime(ResultSet["hiredate"]);
+                        string TeacherSalary = ResultSet["salary"].ToString();
+
+                        SelectedTeacher.TeacherId = Id;
+                        SelectedTeacher.TeacherFName = FirstName;
+                        SelectedTeacher.TeacherLName = LastName;
+                        SelectedTeacher.TeacherSalary = TeacherSalary;
+                        SelectedTeacher.TeacherHireDate = TeacherHireDate;
+                        SelectedTeacher.TeacherEmpNu = TeacherEmpNu;
+
+                    }
+                }
+            }
+
+
+            //Return the final list of author names
+            return SelectedTeacher;
+        }
+    }
+}
---
+++ Week 9/Cumulative 1/Cumulative 1/Controllers/TeacherPageController.cs
@@ -0,0 +1,35 @@
+﻿using Cumulative_1.Models;
+using Cumulative_1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cumulative_1.Controllers
+{
+
+    public class TeacherPageController : Controller
+    {
+        // currently relying on the API to retrieve author information
+        // this is a simplified example. In practice, both the AuthorAPI and AuthorPage controllers
+        // should rely on a unified "Service", with an explicit interface
+        private readonly TeacherAPIController _api;
+
+        public TeacherPageController(TeacherAPIController api)
+        {
+            _api = api;
+        }
+
+        //GET : AuthorPage/List
+        public IActionResult List()
+        {
+            List<Teacher> Teachers = _api.ListTeachers();
+            return View(Teachers);
+        }
+
+        //GET : AuthorPage/Show/{id}
+        public IActionResult Show(int id)
+        {
+            Teacher SelectedTeacher = _api.FindTeacher(id);
+            return View(SelectedTeacher);
+        }
+
+    }
+}
